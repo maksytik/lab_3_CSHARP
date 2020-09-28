959133003567fa1255e239de029eb95e61881863
alexsudochakov37@gmail.com <alexsudochakov37@gmail.com>
2020-09-28 21:33:43 +0300

    Добавьте файлы проекта.

---
+++ lab_3_CSHARP/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            complex a, b, c, d;
+
+            double p, fi;
+
+            Console.WriteLine("Введите p числа а:");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите fi числа а:");
+            fi = Convert.ToDouble(Console.ReadLine());
+            a = new complex(p, fi);
+
+            Console.WriteLine("Введите p числа b:");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите fi числа b:");
+            fi = Convert.ToDouble(Console.ReadLine());
+            b = new complex(p, fi);
+
+            Console.WriteLine("Введите p числа c:");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите fi числа c:");
+            fi = Convert.ToDouble(Console.ReadLine());
+            c = new complex(p, fi);
+
+            Console.WriteLine("Введите p числа d:");
+            p = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите fi числа d:");
+            fi = Convert.ToDouble(Console.ReadLine());
+            d = new complex(p, fi);
+
+
+            complex result = complex.pow(b, 3) + (a + b) / (c - a) * d;
+
+            Console.WriteLine("Ответ в тригонометрической форме:");
+            result.printTrig();
+            Console.WriteLine();
+            Console.WriteLine("Ответ в алгебраической форме:");
+            result.printAlg();
+            Console.WriteLine();
+
+            Console.WriteLine("end");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lab_3_CSHARP/complex.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class complex
+    {
+        double p, fi;
+        double a, b;
+        public complex(double p, double fi)
+        {
+            this.p = p;
+            this.fi = fi;
+
+            toAlg();
+        }
+
+        private void toTrig()
+        {
+
+            p = Math.Sqrt(a * a + b * b);
+            fi = Math.Acos(a / p);
+        }
+
+
+        private void toAlg()
+        {
+            a = p * Math.Cos(fi);
+            b = p * Math.Sin(fi);
+
+        }
+
+        public static complex pow(complex x, int n)
+        {
+
+            double newp = Math.Pow(x.p, n);
+            double newfi = n * x.fi;
+            complex newc = new complex(newp, newfi);
+            newc.toAlg();
+            return newc;
+
+        }
+
+        public static complex operator +(complex x, complex y)
+        {
+
+            double newa, newb;
+
+            newa = x.a + y.a;
+            newb = x.b + y.b;
+            complex newc = new complex(0, 0);
+            newc.a = newa;
+            newc.b = newb;
+            newc.toTrig();
+
+            return newc;
+        }
+
+        public static complex operator -(complex x, complex y)
+        {
+
+            double newa, newb;
+
+            newa = x.a - y.a;
+            newb = x.b - y.b;
+            complex newc = new complex(0, 0);
+            newc.a = newa;
+            newc.b = newb;
+            newc.toTrig();
+
+            return newc;
+        }
+
+
+        public static complex operator *(complex x, complex y)
+        {
+            double newp, newfi;
+            newp = x.p * y.p;
+            newfi = x.fi + y.fi;
+            complex newc = new complex(newp, newfi);
+            newc.toAlg();
+            return newc;
+
+        }
+        public static complex operator /(complex x, complex y)
+        {
+            double newp, newfi;
+            newp = x.p / y.p;
+            newfi = x.fi - y.fi;
+            complex newc = new complex(newp, newfi);
+            newc.toAlg();
+            return newc;
+
+        }
+
+        public void printTrig()
+        {
+            Console.Write(p + "+(cos(" + fi + ")+i*sin(" + fi + "))");
+
+
+        }
+
+        public void printAlg()
+        {
+            Console.Write(a + "+i" + b);
+        }
+
+    }
+}
